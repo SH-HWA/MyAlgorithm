52c027c6a9194f34a57e074872cab380be056961
Hyun H <raidi9069@gmail.com>
2022-01-14 10:40:31 +0900

    01/14 2

--- level2/모음사전.js
+++ level2/모음사전.js
@@ -1,24 +1,34 @@
-function solution(word) {
-  let dictionary = [];
+// function solution(word) {
+//     let dictionary = [];
+//     let vowel = ['A', 'E', 'I', 'O', 'U'];
+
+//     function makeWord(str) {
+//         if (str.length >= 5) {
+//             return;
+//         }
+
+//         for (let i = 0; i < vowel.length; i++) {
+//             let newStr = str + vowel[i];
+//             dictionary.push(newStr);
+//             makeWord(newStr);
+//         }
+//     }
+
+//     makeWord('');
+//     return dictionary.indexOf(word) + 1;
+// }
+
+// 사전을 직접 만들어 찾는것 말고 자릿수마다 사전에 5의 거듭제곱씩 더한 수를 더해나가서 계산하는 방법
+// 5의0승 부터 시작되는자리~ / +5~ / +25~ / +125~ / +625~ 인덱스가 아닌 순서니까 0이 아닌 1부터 시작함을 고려
+// [1, 6, 31, 156, 781]
+function solution(words) {
+  let wordArr = words.split("");
   let vowel = ["A", "E", "I", "O", "U"];
-
-  function makeWord(str) {
-    if (str.length >= 5) {
-      return;
-    }
-
-    for (let i = 0; i < vowel.length; i++) {
-      let newStr = str + vowel[i];
-      dictionary.push(newStr);
-      makeWord(newStr);
-    }
+  let idxArr = [1, 6, 31, 156, 781];
+  let answer = 0;
+  for (let i = 0; i < wordArr.length; i++) {
+    answer = answer + idxArr[4 - i] * vowel.indexOf(wordArr[i]) + 1;
   }
 
-  makeWord("");
-  return dictionary.indexOf(word) + 1;
+  return answer;
 }
-
-// 사전을 직접 만들어 찾는것 이외에 자릿수마다 사전에 5의 거듭제곱씩 더한 수를 더해나가서 계산하는 방법도 있다!
-// function solution(words) {
-//     return words.split('').reduce((acc, cur, idx) => acc + [781, 156, 31, 6, 1][idx] * ['A', 'E', 'I', 'O', 'U'].indexOf(cur) + 1, 0);
-// }
